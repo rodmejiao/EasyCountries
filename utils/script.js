3f59409d05817ad31a037c5a276285464475dc35
rodmejiao <hebertmejia28@gmail.com>
2021-06-21 22:54:39 -0500

    doing working each button correct

--- utils/script.js
+++ utils/script.js
@@ -1,15 +1,20 @@
 
-function ShowCountries() {
+function ShowCountries(continent) {
+    let continentSelected =continent.getAttribute(`data-continent`)
+    console.log(continentSelected)
     const URL_API=`https://restcountries.eu/rest/v2/all`
+    const URL_API_REGION=`https://restcountries.eu/rest/v2/region/${continentSelected}`
     let xhr=new XMLHttpRequest()
-    xhr.open('GET',URL_API,true)
+    xhr.open('GET',URL_API_REGION,true)
         xhr.onload=function () {
             if (xhr.status==200) {
                 console.log(`success`)
                 let countries=JSON.parse(this.response)
-                let countriesByContinent=countries.filter(country => country.region==`Europe` )
+              /*let countriesByContinent=countries.filter(country => country.region==`Europe` ) */
                 let mainCountries=document.getElementsByClassName(`countries`)[0]
-                countriesByContinent.forEach(country => {
+                mainCountries.innerHTML=""
+              /*countriesByContinent.forEach(country => { */
+                    countries.forEach(country => {
                     const CARD= document.createElement(`div`)
                     const NAME_COUNTRY=document.createElement(`h2`)
                     const FLAG_COUNTRY=document.createElement(`img`)
