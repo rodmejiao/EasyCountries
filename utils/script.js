e08c76d5dd1fdc6f62c0d3b5fbd3dc9fec8b3173
rodmejiao <hebertmejia28@gmail.com>
2021-06-29 20:46:01 -0500

    trying loader v3

--- utils/script.js
+++ utils/script.js
@@ -1,8 +1,6 @@
 
 function ShowCountries(continent) {
-    let continentSelected =continent
-
-
+    let continentSelected =continent.getAttribute(`data-continent`)
    /*  let continentSelected =continent.getAttribute(`data-continent`) */
     const URL_API=`https://restcountries.eu/rest/v2/all`
     const URL_API_REGION=`https://restcountries.eu/rest/v2/region/${continentSelected}`
@@ -10,10 +8,6 @@ function ShowCountries(continent) {
     xhr.open('GET',URL_API_REGION,true)
         xhr.onload=function () {
             if (xhr.status==200) {
-                let loader=document.getElementsByClassName(`loader`)[0]
-                loader.style.display=`block`
-                console.log(`loader start`)
-
                 let countries=JSON.parse(this.response)
               /*let countriesByContinent=countries.filter(country => country.region==`Europe` ) */
                 let mainCountries=document.getElementsByClassName(`countries`)[0]
@@ -43,7 +37,7 @@ function ShowCountries(continent) {
                     CARD_INFO_CAPITAL_INFO.textContent=`Show Data`
                     CARD_INFO_CAPITAL_INFO.href=`./countryDetail.html?name=${country.name}`
 
-                    
+                    console.log(`print`)
                     CONTAINER_FLAG_COUNTRY.appendChild(FLAG_COUNTRY)
                     CARD.appendChild(CONTAINER_FLAG_COUNTRY)
                     CARD.appendChild(NAME_COUNTRY)
@@ -51,13 +45,33 @@ function ShowCountries(continent) {
                     CARD_INFO.appendChild(CARD_INFO_CAPITAL_COUNTRY)
                     CARD_INFO.appendChild(CARD_INFO_CAPITAL_INFO)
                     CARD.appendChild(CARD_INFO)
-                    mainCountries.appendChild(CARD)
-                    
+                    mainCountries.appendChild(CARD)                
                 });
             }
-            let loader=document.getElementsByClassName(`loader`)[0]
-            loader.style.display=`none`
-            console.log(`loader end`)
         }
     xhr.send()
 }
+
+
+function demorona() {
+    return new Promise(resolve => {
+        setTimeout(() => {
+          resolve('rodriguito');
+        }, 2000);
+      });
+}
+
+
+async function asyncCall(continent) {
+    let loader=document.getElementsByClassName(`countries`)[0]
+    loader.style.width=`400px`
+    loader.style.height=`400px`
+    loader.style.background=`red`
+
+    const rodrigo = await ShowCountries(continent)
+
+    console.log(rodrigo)
+
+    loader.style.background=`blue`
+
+  }
